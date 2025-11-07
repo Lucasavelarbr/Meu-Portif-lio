8b39c0b08344e684eb86090f58bbc5ade04cf5dc
Lucas Brasil <lucas_2603@hotmail.com>
2025-11-07 16:06:40 -0300

    Novas melhorias: Navegação 100% por teclado e alteração de menu lateral

--- script.js
+++ script.js
@@ -3,5 +3,24 @@ const toggleMenu = document.getElementById("toggle-menu")
 const menu = document.querySelector(".menu-lateral")
 
 toggleMenu.addEventListener("click", () =>{
+    
     menu.classList.toggle("open");
 });
+
+// Fecha o menu ao clicar fora 
+
+document.addEventListener("click", (e) => {
+    const ClicksideMenu = menu.contains(e.target)
+    const ClickButton = toggleMenu.contains(e.target)
+
+    if (!ClicksideMenu && !ClickButton) {
+        menu.classList.remove("open")
+    } 
+})
+
+document.addEventListener("keydown", (e) =>{
+    if (e.key === "Escape"){
+        menu.classList.remove("open")
+    } 
+})
+
